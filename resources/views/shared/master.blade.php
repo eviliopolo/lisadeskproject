37aab3472265fd31449debb7e2cab3fe43302765
Evilio Polo M <evilio.polo@omnia.com.co>
2019-01-15 14:23:40 -0500

    Se aplica plantilla de Boostrap para el diseño de usuario final

---
+++ resources/views/shared/master.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>@yield('title')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+
+    <title>Album example for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('ld_theme/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('ld_theme/css/album.css')}} " rel="stylesheet">
+    <link href="{{asset('ld_theme/css/starter-template.css')}}" rel="stylesheet">
+  </head>
+
+  <body>
+
+    @include('shared.navbar')
+    @yield('content')
+
+    
+    <footer class="text-muted">
+      <div class="container">
+        <p class="float-right">
+          <a href="#">Back to top</a>
+        </p>
+        <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+        <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="{{asset('ld_theme/js/jquery-slim.min.js')}} "><\/script>')</script>
+    <script src=" {{ asset('ld_theme/js/popper.min.js') }}"></script>
+    <script src=" {{ asset ('ld_theme/js/bootstrap.min.js')}}"></script>
+    <script src=" {{ asset('ld_theme/js/holder.min.js') }}"></script>
+  </body>
+</html>
